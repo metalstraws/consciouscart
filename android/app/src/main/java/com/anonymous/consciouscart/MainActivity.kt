bdfe0cd44e92fdfcd64051d681c0efef2041940f
Ryan <ryanshaunsmith@gmail.com>
2024-11-11 11:15:12 +0000

    regenerate the native directories with URI schemes

--- android/app/src/main/java/com/anonymous/consciouscart/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/consciouscart/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.consciouscart
+package com.anonymous.consciouscart
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/anonymous/consciouscart/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/consciouscart/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.consciouscart
+package com.anonymous.consciouscart
 
 import android.app.Application
 import android.content.res.Configuration
