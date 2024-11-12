799aa19c9c33f69b2fd13e6f0721cfdcf37858b2
Ryan <ryanshaunsmith@gmail.com>
2024-11-12 13:54:07 +0000

    add shadow and fetched data

--- components/ProductInfoCard/ProductInfoCard.tsx
+++ components/ProductInfoCard/ProductInfoCard.tsx
@@ -1,15 +1,48 @@
-import React from "react";
-import {View, Text, StyleSheet, FlatList} from "react-native";
+import React, {useState, useEffect} from "react";
+import {View, Text, Image, StyleSheet, FlatList} from "react-native";
+
+import fetchData from "@/app/utils/fetchData";
 
 export default function ProductInfoCard() {
+
+    interface Product {
+        product_name: string;
+        brands: string;
+        packaging: string | null;
+        ecoscore_score: number;
+        stores: string;
+    }
+
+    const [product, setProduct] = useState<Product | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+    const getProducts = async () => {
+        try {
+        const data = await fetchData();
+        setProduct(data);
+        } catch (error) {
+        console.error('Error fetching data:', error);
+        } finally {
+        setLoading(false);
+        }
+    };
+    
+    getProducts();
+    }, []);
+
     return(
         <View style={styles.container}>
-            <Text>Product Name: Ketchup</Text>
-            <Text>Brand: Heinz</Text>
-            <Text>Packaging: Metal</Text>
-            <Text>EcoScore: 65</Text> 
-            <Text>Available Stores: Tesco, M&S</Text>
-            <Text>Image here</Text> 
+            {loading? (<Text>Loading...</Text>) : (
+            <View>
+                <Text>Product Name: {product?.product_name}</Text>
+                <Text>Brand: {product?.brands}</Text>
+                <Text>Packaging: {!product?.packaging ? "no packaging information available" : product?.packaging}</Text>
+                <Text>EcoScore: {product?.ecoscore_score}</Text>
+                <Text>Available Stores: {product?.stores}</Text>
+                <Text>Image here</Text>
+            </View>
+            )}
         </View>
     )
 }
@@ -24,5 +57,10 @@ const styles = StyleSheet.create({
         marginTop: 50,
         borderRadius: 10,
         gap: 5,
+        shadowColor: '#171717',
+        shadowOffset: {width: 0, height: 3},
+        shadowOpacity: 0.4,
+        shadowRadius: 2,
+        elevation: 5, // Android only
     }
 })
